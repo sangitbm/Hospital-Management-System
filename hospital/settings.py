ad7c1b539cde5d48fcfa3c4a3bb1145e0aa97cf1
Sangit BM <sangitbmktm@gmail.com>
2026-09-01 12:49:28 +0545

    update

--- hospital/settings.py
+++ hospital/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m9t!h2wrh&9j-jo!b$htg&b_-oj3_=6ko5&3s!fg+yo50=)3am'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
